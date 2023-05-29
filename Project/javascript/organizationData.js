d547149cb7817f67984607f1914a6a467bb32692
EJLee1209 <dldmswo1209@gmail.com>
2023-05-29 19:09:15 +0900

    봉사활동 페이지 데이터 연결

---
+++ Project/javascript/organizationData.js
@@ -0,0 +1,159 @@
+var all = [
+  {
+    title: '고래의 앞길을 막는 우드사이드의 가스 탐사',
+    description:
+      '우드사이드 에너지는 위험한 탄성파 탐사와 가스 채굴로 고래의 안식처를 위협하고 있습니다',
+    image:
+      'https://www.greenpeace.org/static/planet4-korea-stateless/2022/11/c0c37102-gp1sub1a_medium_res-e1684200342824.jpg',
+  },
+  {
+    title: '테스트2',
+    description: '설명2',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트3',
+    description: '설명3',
+    image: '../assets/image/industry.png',
+  },
+  {
+    title: '테스트4',
+    description: '설명4',
+    image: '../assets/image/childrens.jpg',
+  },
+  {
+    title: '테스트5',
+    description: '설명5',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트6',
+    description: '설명6',
+    image: '../assets/image/icebear.jpg',
+  },
+  {
+    title: '테스트7',
+    description: '설명7',
+    image: '../assets/image/industry.png',
+  },
+  {
+    title: '테스트8',
+    description: '설명8',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트9',
+    description: '설명9',
+    image: '../assets/image/childrens.jpg',
+  },
+  {
+    title: '테스트10',
+    description: '설명10',
+    image: '../assets/image/icebear.jpg',
+  },
+];
+
+var domestic = [
+  {
+    title: '테스트11',
+    description: '설명1',
+    image: '../assets/image/icebear.jpg',
+  },
+  {
+    title: '테스트12',
+    description: '설명2',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트13',
+    description: '설명3',
+    image: '../assets/image/industry.png',
+  },
+  {
+    title: '테스트14',
+    description: '설명4',
+    image: '../assets/image/childrens.jpg',
+  },
+  {
+    title: '테스트15',
+    description: '설명5',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트16',
+    description: '설명6',
+    image: '../assets/image/icebear.jpg',
+  },
+  {
+    title: '테스트17',
+    description: '설명7',
+    image: '../assets/image/industry.png',
+  },
+  {
+    title: '테스트18',
+    description: '설명8',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트19',
+    description: '설명9',
+    image: '../assets/image/childrens.jpg',
+  },
+  {
+    title: '테스트20',
+    description: '설명10',
+    image: '../assets/image/icebear.jpg',
+  },
+];
+var foreign = [
+  {
+    title: '테스트21',
+    description: '설명1',
+    image: '../assets/image/icebear.jpg',
+  },
+  {
+    title: '테스트22',
+    description: '설명2',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트23',
+    description: '설명3',
+    image: '../assets/image/industry.png',
+  },
+  {
+    title: '테스트24',
+    description: '설명4',
+    image: '../assets/image/childrens.jpg',
+  },
+  {
+    title: '테스트25',
+    description: '설명5',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트26',
+    description: '설명6',
+    image: '../assets/image/icebear.jpg',
+  },
+  {
+    title: '테스트27',
+    description: '설명7',
+    image: '../assets/image/industry.png',
+  },
+  {
+    title: '테스트28',
+    description: '설명8',
+    image: '../assets/image/sea.jpg',
+  },
+  {
+    title: '테스트29',
+    description: '설명9',
+    image: '../assets/image/childrens.jpg',
+  },
+  {
+    title: '테스트30',
+    description: '설명10',
+    image: '../assets/image/icebear.jpg',
+  },
+];
