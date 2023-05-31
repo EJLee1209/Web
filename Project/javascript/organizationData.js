cb95b340389da18c55eb1e6ba0c1298cc02490a7
EJLee1209 <dldmswo1209@gmail.com>
2023-05-31 13:57:28 +0900

    단체 상세페이지 데이터 연결

--- Project/javascript/organizationData.js
+++ Project/javascript/organizationData.js
@@ -1,159 +1,54 @@
-var all = [
+var organizations = [
   {
-    title: '고래의 앞길을 막는 우드사이드의 가스 탐사',
-    description:
-      '우드사이드 에너지는 위험한 탄성파 탐사와 가스 채굴로 고래의 안식처를 위협하고 있습니다',
-    image:
-      'https://www.greenpeace.org/static/planet4-korea-stateless/2022/11/c0c37102-gp1sub1a_medium_res-e1684200342824.jpg',
+    title: '그린피스',
+    description: '그린피스는 지구의 목소리를 대신하기 위해 활동합니다',
+    thumbnail:
+      'https://mblogthumb-phinf.pstatic.net/MjAxODAxMjRfMTQy/MDAxNTE2NzU1Njc2NzYw.Q8frvGxyttKZcpwO7kakFSiPrCiHxBqR6Zm5cKv3c3cg.opDLvP0b7ZqTvzqfhZ_d77d_fGGpy0RI_XdRaRsX99sg.JPEG.keiti_sns/01.%EA%B7%B8%EB%A6%B0%ED%94%BC%EC%8A%A4.jpg?type=w800',
+    images: [
+      '../assets/image/그린피스1.png',
+      '../assets/image/그린피스2.png',
+      '../assets/image/그린피스3.png',
+      '../assets/image/그린피스4.png',
+    ],
+    introduce:
+      "그린피스는 전 지구적인 환경문제의 원인을 밝혀내고, 이를 해결하기 위해 평화적이고 창의적인 방식으로 활동하는 캠페인 단체입니다. 그린피스는 지구를 위한 변화를 만드는 가장 큰 힘은 바로 '시민의 힘'이라고 믿습니다.",
+    donateDescription:
+      '그린피스는 정치, 재정적 독립성을 위해 개인의 후원으로만 운영됩니다. 여러분의 후원으로 주요 환경문제에 맞설 수 있습니다.',
+    link: 'https://www.greenpeace.org/korea/',
+    type: 'foreign',
   },
   {
-    title: '테스트2',
-    description: '설명2',
-    image: '../assets/image/sea.jpg',
+    title: '환경운동연합',
+    description: '30년간 생명을 지켜온 아시아 최대 규모의 환경단체',
+    thumbnail:
+      'https://pbs.twimg.com/profile_images/1161200922170544129/Ee2567A6_400x400.jpg',
+    images: [
+      'http://kfem.or.kr/wp-content/themes/kfem/images/page/c01.jpg',
+      'http://kfem.or.kr/wp-content/themes/kfem/images/page/c02.jpg',
+      'http://kfem.or.kr/wp-content/themes/kfem/images/page/c03.jpg',
+      'http://kfem.or.kr/wp-content/themes/kfem/images/page/c06.jpg',
+    ],
+    introduce:
+      '환경운동연합은 창립 이후 지금까지 현장성, 대중성, 전문성을 바탕으로  아시아 최대의 환경단체로 성장했습니다. 1990년대 지리산·점봉산·덕유산보호, 시화호 살리기, 동강 살리기(동강댐 저지), 가야산 골프장 저지, 핵폐기장 강행저지, 새만금 살리기, 서·남해안 습지 보전, 비무장지대 보호, 팔당 상수원 보호, 낙동강 살리기, 시민 대기 조사, 대만 핵폐기물 반대운동, 그리고 2000년대 천성산 살리기, 계룡산 국립공원 관통도로 백지화, 부안 핵폐기장 백지화, 청주 원흥이 두꺼비 서식지 보전, 서해안 기름유출사고 대응, 한반도대운하 백지화, 후쿠시마 방사능 누출 대응까지 전국의 환경 현장에서 역할을 다하기 위해 노력하고 있습니다. 현재 환경연합에는 현장을 지키는 전국의 풀뿌리 지역조직 54개와 시민환경연구소, 환경법률센터, 월간 함께사는 길, 에코생협, 시민환경정보센터 등 전문성과 대중성을 높이기 위한 전문기관과 협력기관들이 있습니다.또한 핵물질 이용 반대, 멸종위기종 및 고래 보호, 기후변화 대응과 같은 지구환경문제 해결을 위하여 리우회의 참석 등 국제 연대에 적극적으로 참여하고 있습니다. 2002년에는 세계 3대 환경단체 중 하나인 ‘지구의 벗’ 회원단체로 정식 가입하여 ‘지구의 벗 한국’으로 지구적 책임을 다하기 위해 협력하고 있습니다.',
+    donateDescription:
+      '지난 30년 동안 환경과 생명을 지키기 위해 가장 많은 곳을 누빈 그 열정 그대로. 환경운동연합은 8만 회원, 54개 지역, 5개 전문기관, 8개 협력기관과 함께 전국 환경이슈의 현장 속에 그리고 당신의 생활 속에 늘 함께하겠습니다.',
+    link: 'http://kfem.or.kr/',
+    type: 'domestic',
   },
   {
-    title: '테스트3',
-    description: '설명3',
-    image: '../assets/image/industry.png',
-  },
-  {
-    title: '테스트4',
-    description: '설명4',
-    image: '../assets/image/childrens.jpg',
-  },
-  {
-    title: '테스트5',
-    description: '설명5',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트6',
-    description: '설명6',
-    image: '../assets/image/icebear.jpg',
-  },
-  {
-    title: '테스트7',
-    description: '설명7',
-    image: '../assets/image/industry.png',
-  },
-  {
-    title: '테스트8',
-    description: '설명8',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트9',
-    description: '설명9',
-    image: '../assets/image/childrens.jpg',
-  },
-  {
-    title: '테스트10',
-    description: '설명10',
-    image: '../assets/image/icebear.jpg',
-  },
-];
-
-var domestic = [
-  {
-    title: '테스트11',
-    description: '설명1',
-    image: '../assets/image/icebear.jpg',
-  },
-  {
-    title: '테스트12',
-    description: '설명2',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트13',
-    description: '설명3',
-    image: '../assets/image/industry.png',
-  },
-  {
-    title: '테스트14',
-    description: '설명4',
-    image: '../assets/image/childrens.jpg',
-  },
-  {
-    title: '테스트15',
-    description: '설명5',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트16',
-    description: '설명6',
-    image: '../assets/image/icebear.jpg',
-  },
-  {
-    title: '테스트17',
-    description: '설명7',
-    image: '../assets/image/industry.png',
-  },
-  {
-    title: '테스트18',
-    description: '설명8',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트19',
-    description: '설명9',
-    image: '../assets/image/childrens.jpg',
-  },
-  {
-    title: '테스트20',
-    description: '설명10',
-    image: '../assets/image/icebear.jpg',
-  },
-];
-var foreign = [
-  {
-    title: '테스트21',
-    description: '설명1',
-    image: '../assets/image/icebear.jpg',
-  },
-  {
-    title: '테스트22',
-    description: '설명2',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트23',
-    description: '설명3',
-    image: '../assets/image/industry.png',
-  },
-  {
-    title: '테스트24',
-    description: '설명4',
-    image: '../assets/image/childrens.jpg',
-  },
-  {
-    title: '테스트25',
-    description: '설명5',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트26',
-    description: '설명6',
-    image: '../assets/image/icebear.jpg',
-  },
-  {
-    title: '테스트27',
-    description: '설명7',
-    image: '../assets/image/industry.png',
-  },
-  {
-    title: '테스트28',
-    description: '설명8',
-    image: '../assets/image/sea.jpg',
-  },
-  {
-    title: '테스트29',
-    description: '설명9',
-    image: '../assets/image/childrens.jpg',
-  },
-  {
-    title: '테스트30',
-    description: '설명10',
-    image: '../assets/image/icebear.jpg',
+    title: '녹색연합',
+    description: '우리는 생명존중, 생태순환사회, 비폭력평화, 녹색자치의 가치를 닮아갑니다.',
+    thumbnail:
+      'https://www.ngonews.kr/news/photo/202204/133903_62748_3052.jpg',
+    images: [
+      'https://www.greenkorea.org/wp-content/uploads/2020/02/07164447/intro-04.jpg',
+      'https://www.greenkorea.org/wp-content/uploads/2020/02/07164445/intro-02.jpg',
+    ],
+    introduce:
+      '녹색연합은 1991년 창립하여 우리나라 자연을 지키는 환경단체입니다. 주요 생태축인 백두대간과 DMZ를 보전하고 야생동물과 그들의 서식지를 지킵니다. 기후위기를 가속화하는 현장을 감시하며 에너지가 정의로운 세상, 쓰레기가 없는 지구, 자연과 사람이 조화로운 사회를 그려갑니다.',
+    donateDescription:
+      '혼자서는 해결하기 힘든 환경문제, 한 사람 한 사람의 꾸준한 관심과 참여가 모여야 합니다. 여러분이 직접 현장에 갈 수 없다면, 녹색연합을 후원해주세요. 녹색연합이 대신 현장을 누비며 당신과 자연을 잇겠습니다. 당신의 일상이 지구를 살리는 방식으로 변화되도록 돕겠습니다.',
+    link: 'https://www.greenkorea.org/support/',
+    type: 'domestic',
   },
 ];
--- Project/javascript/organizationDetail.js
+++ Project/javascript/organizationDetail.js
@@ -1,73 +1,128 @@
-const carousel = document.querySelector(".carousel"),
-firstImg = carousel.querySelectorAll("img")[0],
-arrowIcons = document.querySelectorAll(".wrapper i");
+// JavaScript로 쿼리 파라미터 값을 가져오는 함수
+function getQueryParams() {
+  var params = {};
+  var url = window.location.href;
+  var queryString = url.split('?')[1];
 
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+  if (queryString) {
+    var keyValuePairs = queryString.split('&');
+    for (var i = 0; i < keyValuePairs.length; i++) {
+      var keyValue = keyValuePairs[i].split('=');
+      var key = decodeURIComponent(keyValue[0]);
+      var value = decodeURIComponent(keyValue[1]);
+      params[key] = value;
+    }
+  }
 
-const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+  return params;
 }
 
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-    });
-});
+$(document).ready(() => {
+  // 가져온 데이터로 페이지 로드
+  var queryParams = getQueryParams();
+  var selectedIdx = queryParams['selectedIdx'];
 
-const autoSlide = () => {
+  // images
+  organizations[selectedIdx].images.forEach((element) => {
+    $(`<img src="${element}"/>`).appendTo('#imageContainer');
+  });
+
+  // introduce
+  $(
+    `<div id=description-box>${organizations[selectedIdx].introduce}</div>`
+  ).replaceAll('#description-box');
+  // link
+  $('#donateBtn').attr('href', organizations[selectedIdx].link);
+  // donate description
+  $(
+    `<div id=donateDescription>${organizations[selectedIdx].donateDescription}</div>`
+  ).replaceAll('#donateDescription');
+
+  // 슬라이드 이미지 설정
+  const carousel = document.querySelector('.carousel'),
+    firstImg = carousel.querySelectorAll('img')[0],
+    arrowIcons = document.querySelectorAll('.wrapper i');
+
+  let isDragStart = false,
+    isDragging = false,
+    prevPageX,
+    prevScrollLeft,
+    positionDiff;
+
+  const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? 'none' : 'block';
+    arrowIcons[1].style.display =
+      carousel.scrollLeft == scrollWidth ? 'none' : 'block';
+  };
+
+  arrowIcons.forEach((icon) => {
+    icon.addEventListener('click', () => {
+      let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+      // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+      carousel.scrollLeft += icon.id == 'left' ? -firstImgWidth : firstImgWidth;
+      setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
+  });
+
+  const autoSlide = () => {
     // if there is no image left to scroll then return from here
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    if (
+      carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) >
+        -1 ||
+      carousel.scrollLeft <= 0
+    )
+      return;
 
     positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
     let firstImgWidth = firstImg.clientWidth + 14;
     // getting difference value that needs to add or reduce from carousel left to take middle img center
     let valDifference = firstImgWidth - positionDiff;
 
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    if (carousel.scrollLeft > prevScrollLeft) {
+      // if user is scrolling to the right
+      return (carousel.scrollLeft +=
+        positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
     }
     // if user is scrolling to the left
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-}
+    carousel.scrollLeft -=
+      positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+  };
 
-const dragStart = (e) => {
+  const dragStart = (e) => {
     // updatating global variables value on mouse down event
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
-}
+  };
 
-const dragging = (e) => {
+  const dragging = (e) => {
     // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
+    if (!isDragStart) return;
     e.preventDefault();
     isDragging = true;
-    carousel.classList.add("dragging");
+    carousel.classList.add('dragging');
     positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
     showHideIcons();
-}
+  };
 
-const dragStop = () => {
+  const dragStop = () => {
     isDragStart = false;
-    carousel.classList.remove("dragging");
+    carousel.classList.remove('dragging');
 
-    if(!isDragging) return;
+    if (!isDragging) return;
     isDragging = false;
     autoSlide();
-}
+  };
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
+  carousel.addEventListener('mousedown', dragStart);
+  carousel.addEventListener('touchstart', dragStart);
 
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
+  document.addEventListener('mousemove', dragging);
+  carousel.addEventListener('touchmove', dragging);
 
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
+  document.addEventListener('mouseup', dragStop);
+  carousel.addEventListener('touchend', dragStop);
+});
